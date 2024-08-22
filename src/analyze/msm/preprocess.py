bf7532e7d7e0ee38d4749ece6619f1363ec3e10e
Archit Vasan <avasan@lambda0.cels.anl.gov>
2024-08-22 12:06:25 -0700

    added preprocess for multiple trajectories

--- src/analyze/msm/preprocess.py
+++ src/analyze/msm/preprocess.py
@@ -14,7 +14,7 @@ from deeptime.markov.msm import BayesianMSM
 import deeptime.markov as markov
 import matplotlib.pyplot as plt
 
-def main(datafile, 
+def _main(datafile, 
             numclusts,
             out_dtrajs,
             out_data,
@@ -78,13 +78,86 @@ def main(datafile,
     plt.close()
     return
 
+def main(datafilelist, 
+            numclusts,
+            out_dtrajs,
+            out_data,
+            Timescale_image,
+            discrete_image,
+            ck_image,
+            device="0"):
+
+    if torch.cuda.is_available():
+        device = torch.device(f"cuda:{device}")
+        torch.backends.cudnn.benchmark = True
+    else:
+        device = torch.device("cpu")
+    torch.set_num_threads(12)
+    
+    data = [] 
+    for datafile in datafilelist:
+        data_it = np.load(datafile)#"./run-4/selection_runs/selection-0/sd4_projection.npy")
+        data = data.extend([data_it.astype(np.float32)])
+    
+    dataset = TrajectoriesDataset.from_numpy(1, data)
+
+    cluster = KMeans(numclusts, progress=tqdm).fit_fetch(data)
+    dtrajs = [cluster.transform(x) for x in data]
+    msm_estimator = markov.msm.MaximumLikelihoodMSM(
+                    reversible=True,
+                    stationary_distribution_constraint=None
+                    )
+    
+    lagtimes = np.arange(1, 30, dtype=np.int32)
+    models = [msm_estimator.fit(dtrajs, lagtime=lag).fetch_model() for lag in tqdm(lagtimes)]
+    
+    ax = plot_implied_timescales(implied_timescales(models))
+    ax.set_yscale('log')
+    ax.set_xlabel('lagtime')
+    ax.set_ylabel('timescale')
+    plt.savefig(Timescale_image)
+    plt.close()
+    
+    
+    f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+    np.savetxt(out_dtrajs, np.array(dtrajs).T, fmt='%i')
+    np.savetxt(out_data, data[0])
+    #counts,ybins,xbins,image = np.histogram2d(projections[0][:,0], projections[0][:,1], bins=100, norm=LogNorm())
+    cb = ax2.hist2d(data[0][:,0], data[0][:,1], bins=20)
+    #plt.colorbar(cb, ax=ax2)
+    ax1.plot(dtrajs)
+    ax1.set_xlim(0,40000)
+    ax1.set_xlabel('t')
+    ax1.set_ylabel('state')
+    ax2.set_xlabel('SD4-1')
+    ax2.set_ylabel('SD4-2')
+    plt.savefig(discrete_image)
+    plt.close()
+    
+    dtrajs = np.loadtxt(out_dtrajs)
+    dtrajs = dtrajs.astype(np.int32)
+    bmsms = [BayesianMSM(lagtime=lag).fit_fetch(dtrajs) for lag in tqdm([10, 20, 25, 30, 40, 50])]#, 60, 70, 80, 90, 100])]
+    ck_test = bmsms[5].ck_test(bmsms, 6)
+    plot_ck_test(ck_test)
+    plt.savefig(ck_image)
+    plt.close()
+    return
+
+
+
 if __name__ == "__main__":
+    # Define a custom argument type for a list of strings
+
+    def list_of_strings(arg):
+        return arg.split(',')
+
+
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-D',
-                        '--datafile',
-                        type=str,
-                        help='datafile from heiranca script')
+                        '--datafilelist',
+                        type=list_of_strings,
+                        help='datafile list from heiranca script')
 
     parser.add_argument('-c',
                         '--numclusts',
@@ -107,6 +180,11 @@ if __name__ == "__main__":
                         required=False,
                         default="0",
                         help='cuda device')
+    
+    #parser.add_argument('--str-list',
+    #                    type=list_of_strings)
+
+    
 
     args = parser.parse_args()
 
@@ -121,7 +199,7 @@ if __name__ == "__main__":
     except:
         pass
 
-    main(args.datafile, 
+    main(args.datafilelist, 
             args.numclusts,
             f'{args.outdir}/dtrajs.dat',
             f'{args.outdir}/data.dat',
